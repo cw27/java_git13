434f50786ad2ad25a0ca2af3b30259bb617eb1cf
im222b <rudgnsdleh@naver.com>
2023-05-02 13:12:41 +0900

    이벹트

---
+++ src/event/EventServiceImpl.java
@@ -0,0 +1,17 @@
+package event;
+
+public class EventServiceImpl implements EventService {
+
+	@Override
+	public void event1() {
+		// TODO Auto-generated method stub
+		System.out.println("이번달 이벤트 보기");
+	}
+
+	@Override
+	public void event2() {
+		// TODO Auto-generated method stub
+		System.out.println("전체 이번테 보기");
+	}
+
+}
